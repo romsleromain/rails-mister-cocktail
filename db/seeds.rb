44c7cd3c00dfcac78f3e85d9727fb76291446ce8
Romain Bertrand <bertrand_romain@live.fr>
2019-02-15 13:01:33 +0100

    add gem json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
-#
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Ingredient.create(name: "lemon")
 # Ingredient.create(name: "ice")
 # Ingredient.create(name: "mint leaves")
 
-# require 'json'
-# require 'open-uri'
+require 'json'
+require 'open-uri'
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# user_serialized = open(url).read
-# hash_drinks = JSON.parse(user_serialized)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+hash_drinks = JSON.parse(user_serialized)
 
-# hash_drinks["drinks"].each do |hash|
-#   Ingredient.create(name: hash["strIngredient1"])
-# end
+hash_drinks["drinks"].each do |hash|
+  Ingredient.create(name: hash["strIngredient1"])
+end
 
 
 Cocktail.create(name: "bloody mary")
